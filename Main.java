04da3ef6bd7e30462c1eb2073f3ca5dda28a42a9
Grogl <ziga2003.groselj@gmail.com>
2021-12-21 12:17:42 +0100

    removed KeyGen from Main

--- Main.java
+++ Main.java
@@ -7,29 +7,4 @@ public class Main {
        System.out.println(KeyGen());
 
     }
-
-    public static int KeyGen(){
-        Random r = new Random();
-        int d1 = r.nextInt(9);
-        int d2 = r.nextInt(9);
-        int d3 = r.nextInt(9);
-        int d4 = r.nextInt(9);
-        while(d1==d2||d1==d3||d1==d4||d2==d3||d2==d4||d3==d4)
-        {
-            if(d1==d2||d2==d3||d2==d4)
-            {
-                d2 = r.nextInt(9);
-            }
-            if(d1==d3||d2==d3||d3==d4)
-            {
-                d3 = r.nextInt(9);
-            }
-            if(d1==d4||d2==d4||d3==d4)
-            {
-                d4 = r.nextInt(9);
-            }
-        }   
-        int Key = d1*1000+d2*100+d3*10+d4;
-        return Key;
-            }
 }
