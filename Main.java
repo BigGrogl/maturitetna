de1ce9556220b3b293d4ae542a3ad0b2f420b0f1
Grogl <ziga2003.groselj@gmail.com>
2021-12-21 12:30:24 +0100

    small changes

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args){
-        KeyGen Gen = new KeyGen();
         int Key = KeyGen.KeyGen();
 
        System.out.println(Key);
