b1fc9c4569ce838f412a8e8bdea36de2ba38b0fe
Grogl <ziga2003.groselj@gmail.com>
2021-12-21 11:55:50 +0100

    changed KeyGen

--- Main.java
+++ Main.java
@@ -2,16 +2,33 @@ import java.util.*;
 public class Main {
     public static void main(String[] args){
         
-        Random rand = new Random();
-        int max = 10000;
-        int min = 1000;
-
-        int Key = rand.nextInt(max-min)+min;
-        if(Key%1111==0){
-        Key = rand.nextInt(max-min)+min;
-        }
+        
 
         System.out.println(Key);
 
     }
+
+    public static int KeyGen(){
+        Random r = new Random();
+        int d1 = r.nextInt(9);
+        int d2 = r.nextInt(9);
+        int d3 = r.nextInt(9);
+        int d4 = r.nextInt(9);
+        while(d1==d2||d1==d3||d1==d4||d2==d3||d2==d4||d3==d4)
+        {
+            if(d1==d2||d2==d3||d2==d4)
+            {
+                d2 = r.nextInt(9);
+            }
+            if(d1==d3||d2==d3||d3==d4)
+            {
+                d3 = r.nextInt(9);
+            }
+            if(d1==d4||d2==d4||d3==d4)
+            {
+                d4 = r.nextInt(9);
+            }
+        }   
+        System.out.println(d1+""+d2+""+d3+""+d4);
+            }
 }
