15a99f814a0bbec4405cdc790773cf3f21c1bfaa
Grogl <ziga2003.groselj@gmail.com>
2022-01-04 12:08:26 +0100

    Started work on shifter

--- KeyGen.java
+++ KeyGen.java
@@ -5,23 +5,19 @@ public class KeyGen {
         int d1 = r.nextInt(9);
         int d2 = r.nextInt(9);
         int d3 = r.nextInt(9);
-        int d4 = r.nextInt(9);
-        while(d1==d2||d1==d3||d1==d4||d2==d3||d2==d4||d3==d4)
+        while(d1==d2||d1==d3||d2==d3)
         {
-            if(d1==d2||d2==d3||d2==d4||d2==0)
+            if(d1==d2||d2==d3||d2==0)
             {
                 d2 = r.nextInt(9);
             }
-            if(d1==d3||d2==d3||d3==d4||d3==0)
+            if(d1==d3||d2==d3||d3==0)
             {
                 d3 = r.nextInt(9);
             }
-            if(d1==d4||d2==d4||d3==d4||d4==0)
-            {
-                d4 = r.nextInt(9);
-            }
+           
         }   
-        int Key = d1*1000+d2*100+d3*10+d4;
+        int Key = d1*1000+d2*100+d3*10;
         return Key;
             }
 }
--- Shifter.java
+++ Shifter.java
@@ -7,5 +7,29 @@ public class Shifter {
         char[] roll2 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','z'};
         char[] roll3 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','z'};
 
+        //SHIFT 1
+        char last1 = roll1[roll1.length-1];
+        for(int index1 = roll1.length-1; index1 >= 0; index1--){
+            roll1[index1+1]=roll1[index1];
+        }
+        roll1[0]=last1;
+
+        //SHIFT 2
+        char last2 = roll2[roll2.length-1];
+        for(int index2 = roll2.length-1; index2 >= 0; index2--){
+            roll2[index2+1]=roll2[index2];
+        }
+        roll2[0]=last2;
+
+        //SHIFT 2
+        char last3 = roll3[roll3.length-1];
+        for(int index3 = roll3.length-1; index3 >= 0; index3--){
+            roll3[index3+1]=roll3[index3];
+        }
+        roll3[0]=last3;
+
+        System.out.println(roll1);
+        System.out.println(roll2);
+        System.out.println(roll3);
     }
 }
