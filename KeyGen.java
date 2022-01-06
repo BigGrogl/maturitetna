936db2e3df12793cf6bebcc74b5cc73c8f3fd1ba
Grogl <ziga2003.groselj@gmail.com>
2022-01-06 12:28:54 +0100

    Changed KeyGen function, started work on shifter, loosing mind

--- KeyGen.java
+++ KeyGen.java
@@ -7,6 +7,9 @@ public class KeyGen {
         int d3 = r.nextInt(9);
         while(d1==d2||d1==d3||d2==d3)
         {
+            if(d1==0){
+                d1 = r.nextInt(9);
+            }
             if(d1==d2||d2==d3||d2==0)
             {
                 d2 = r.nextInt(9);
@@ -18,6 +21,6 @@ public class KeyGen {
            
         }   
         int Key = d1*1000+d2*100+d3*10;
-        return Key;
+        return d1;
             }
 }
