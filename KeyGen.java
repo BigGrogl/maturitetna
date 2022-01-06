3d24f8575e16785b64d22cd0a3ab000f7817a8c6
Grogl <ziga2003.groselj@gmail.com>
2022-01-06 12:42:22 +0100

    Changed KeyGen and Shifter now working

--- KeyGen.java
+++ KeyGen.java
@@ -2,25 +2,25 @@ import java.util.*;
 public class KeyGen {
     public static int KeyGenerator(){
         Random r = new Random();
-        int d1 = r.nextInt(9);
-        int d2 = r.nextInt(9);
-        int d3 = r.nextInt(9);
-        while(d1==d2||d1==d3||d2==d3)
+        int key1 = r.nextInt(9);
+        int key2 = r.nextInt(9);
+        int key3 = r.nextInt(9);
+        while(key1==key2||key1==key3||key2==key3)
         {
-            if(d1==0){
-                d1 = r.nextInt(9);
+            if(key1==0){
+                key1 = r.nextInt(9);
             }
-            if(d1==d2||d2==d3||d2==0)
+            if(key1==key2||key2==key3||key2==0)
             {
-                d2 = r.nextInt(9);
+                key2 = r.nextInt(9);
             }
-            if(d1==d3||d2==d3||d3==0)
+            if(key1==key3||key2==key3||key3==0)
             {
-                d3 = r.nextInt(9);
+                key3 = r.nextInt(9);
             }
            
         }   
-        int Key = d1*1000+d2*100+d3*10;
-        return d1;
+        int Key = key1*1000+key2*100+key3*10;
+        return key1;
             }
 }
--- Shifter.java
+++ Shifter.java
@@ -8,15 +8,45 @@ public class Shifter {
         char[] roll3 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','z'};
         int Key1 = KeyGen.KeyGenerator();
 
+        //ROLLER 1 LEFT
         for(int i = 0; i < Key1; i++){
             char temp = roll1[0];
-            for(int j = 0; j < roll1.length-1; j++){
+            int j;
+            for(j = 0; j < roll1.length-1; j++){
                 roll1[j]=roll1[j+1];
-                roll1[j]=temp;
             }
+                roll1[j]=temp;
+        }
+
+        //ROLLER 2 RIGHT
+        for (int i = 0; i < Key1; i++) {
+			char temp = roll2[roll1.length - 1];
+			int j;
+			for (j = roll2.length - 1; j > 0; j--){
+				roll2[j] = roll2[j - 1];
+            }
+			roll2[j] = temp;
+		}
+
+        //ROLLER 3 LEFT
+        for(int i = 0; i < Key1; i++){
+            char temp = roll3[0];
+            int j;
+            for(j = 0; j < roll3.length-1; j++){
+                roll3[j]=roll3[j+1];
+            }
+            roll3[j]=temp;
         }
 
         for(int x = 0; x<roll1.length;x++){
+            System.out.print(roll1[x] + " ");
+        }
+        System.out.println();
+        for(int x = 0; x<roll2.length;x++){
+            System.out.print(roll1[x] + " ");
+        }
+        System.out.println();
+        for(int x = 0; x<roll3.length;x++){
             System.out.print(roll1[x] + " ");
         }
     }
