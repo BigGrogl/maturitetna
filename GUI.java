9b3922361ef18a2099b0dd74b3e2c67f512ad057
Grogl <ziga2003.groselj@gmail.com>
2022-01-11 11:03:43 +0100

    update

---
+++ GUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class GUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("TEST GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        JButton button = new JButton("Press");
+        frame.getContentPane().add(button);
+        frame.setVisible(true);
+    }
+}
