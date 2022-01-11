4adccf1748a4e3ba4f8bc4ca99c29f7030b53e42
Grogl <ziga2003.groselj@gmail.com>
2022-01-11 11:54:30 +0100

    a lot of shit

--- Shifter.java
+++ Shifter.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Shifter {
     public static void ArrShift(){
       
-        char[] roll1 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','z'};
+        char[] roll1 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','z',' '};
         char[] roll2 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','z'};
         char[] roll3 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','z'};
         int Key1 = KeyGen.KeyGen1();
