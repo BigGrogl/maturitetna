cb94531dbc52d0a1241a8cf6f69f60e4b1d3b2bd
Grogl <ziga2003.groselj@gmail.com>
2022-03-03 13:24:33 +0100

    REWROTE USE

--- KeyGenShift.java
+++ KeyGenShift.java
@@ -5,7 +5,6 @@ public class KeyGenShift {
     public static void main(String[] args){
 
         int constanta = 52;
-        final char[] cleanArr = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','Z',' ','!','"','?','.',',','(',')'};
         int[] Key = new int[3];
         for(int i = 0; i < 2; i++){
             Key[i] = KeyGen(constanta);
@@ -13,20 +12,23 @@ public class KeyGenShift {
 
         System.out.printf("%d%n%n", constanta);
         System.out.printf("%d %d %d%n", Key[0],Key[1],Key[2]);
-        
-        // Creating array of string length
-        String str = Reader.Read();
-        char[] ch = new char[str.length()];
-
-        // Copy character by character into array
-        for (int i = 0; i < str.length(); i++) {
-            ch[i] = str.charAt(i);
-        }
 
     }
 
     
+    //STRING TO ARRAY
+    public static char[] CharArr(){
+        // Creating array of string length
+        String str = Reader.Read();
+        char[] stringChar = new char[str.length()];
 
+        // Copy character by character into array
+        for (int i = 0; i < str.length(); i++) {
+            stringChar[i] = str.charAt(i);
+        }
+
+        return stringChar;
+    }
 
     //KEY GEN
     public static int KeyGen(int X){
---
+++ encoder.java
@@ -0,0 +1,20 @@
+public class encoder {
+    public static void main(String[] args){
+
+        Encode(KeyGenShift.CharArr());
+
+    }
+
+    public static void Encode(char[] input){
+        
+    }
+
+
+
+
+
+
+
+
+
+}
